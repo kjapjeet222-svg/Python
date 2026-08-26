2dfad181c38a4d483607713fece0e88bda944546
kjapjeet222-svg <kjapjeet222@gmail.com>
2026-08-26 09:08:46 +0100

    Add files via upload

---
+++ activity.py
@@ -0,0 +1,22 @@
+# Printing a greeting message 
+print("hello, world!")
+
+# Printing my name 
+print("My name is Japjeet.")
+
+# Printing a fun fact about myself
+print("I love playing tennis." )
+
+
+# Performing and printing a simple calaculation
+print("The result of 5 + 2 is:", 5 + 2)
+
+
+# My favourite colour 
+print("my favourite colour is red")
+
+# Favourite movie
+print("My favourite movie is James and the giant peach.")
+
+# My dream car
+print("my dream car is mercedes benz.")
