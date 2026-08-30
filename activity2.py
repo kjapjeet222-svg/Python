8a2cfef10f4e6855a982096df7c2a4dba89d13de
kjapjeet222-svg <kjapjeet222@gmail.com>
2026-08-30 16:26:06 +0100

    Add files via upload

---
+++ activity2.py
@@ -0,0 +1,14 @@
+# print a centence
+print("Welcome to the world of Programming.")
+
+#print a number
+print(4)
+
+#next line after print
+print("hello python \n")
+
+#pass multiple arguments for priniting
+print("hello", 5)
+
+#end argument of print statement
+print("welcome to ", end="*")
---
+++ activity3.py
@@ -0,0 +1,13 @@
+# Creating different variables 
+x = 5
+y ="Japjeet"
+print(x)
+print(y)
+
+codingal = "codingal"
+print(codingal)
+
+#user input
+name=input("enter your name: ")
+
+print("\nHello", name, "\nWelcome to codingal")
